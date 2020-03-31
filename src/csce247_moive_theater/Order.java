565f35ea986caee3f8bc674b0fe77463e2513afe
c4181 <cm4181@gmail.com>
2020-03-31 13:37:55 -0400

    Added constructor for existing orders

--- src/csce247_moive_theater/Order.java
+++ src/csce247_moive_theater/Order.java
@@ -2,6 +2,13 @@ package csce247_moive_theater;
 
 import java.util.Date;
 
+/**
+ * This class holds the information for one ticket order
+ * 
+ * @author Christopher Moyer
+ *
+ */
+
 public class Order {
   private Showtime showtime;
   private double amountPaid;
@@ -9,7 +16,7 @@ public class Order {
   private String orderNumber;
 
   /**
-   * Construct a new order
+   * Construct an order with a new order number
    * 
    * @param showtime The showtime being purchased
    * @param amountPaid Total amount the user paid
@@ -23,11 +30,27 @@ public class Order {
   }
 
   /**
+   * Construct an order with an existing order number
+   * 
+   * @param showtime The showtime being purchased
+   * @param amountPaid Total amount the user paid
+   * @param seatNumber The seat number being booked
+   * @param orderNumber The order number for the purchase
+   */
+  public Order(Showtime showtime, double amountPaid, String seatNumber, String orderNumber) {
+    this.showtime = showtime;
+    this.amountPaid = amountPaid;
+    this.seatNumber = seatNumber;
+    this.orderNumber = orderNumber;
+  }
+
+  /**
    * Generates a new order number
    * 
    * @return orderNumber
    */
   private String generateOrderNumber() {
+    // Gets the current Epoch time and adds the seat number to end
     Date now = new Date();
     return String.valueOf(now.getTime()) + seatNumber;
   }
