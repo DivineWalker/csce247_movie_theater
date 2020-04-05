f39bbb20f81d53d46cf2ec60d099063ed36a1c7b
bcrockerT <bcrocker@email.sc.edu>
2020-04-04 22:21:05 -0400

    Added Comments to Event, Movie, Play, Concert

--- src/csce247_moive_theater/Movie.java
+++ src/csce247_moive_theater/Movie.java
@@ -7,6 +7,12 @@ package csce247_moive_theater;
 public class Movie extends Event{
 	private String mpaaRating;
 	
+	/**
+	 * Creates a new Movie listings
+	 * @param name - Name to be used by the super to name the event
+	 */
+	 
+	
 	public Movie(String name){
 		super(name);
 	}
