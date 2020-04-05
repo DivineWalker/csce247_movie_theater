31b72eefcf15c9c929e74e237059b6cdd6828bdb
c4181 <cm4181@gmail.com>
2020-04-05 19:07:08 -0400

    Added getter for name

--- src/csce247_moive_theater/Event.java
+++ src/csce247_moive_theater/Event.java
@@ -1,25 +1,32 @@
 package csce247_moive_theater;
+
 import java.util.ArrayList;
+
 /**
  * 
- * @author brandon
- *	Base Event type, will be extended by different event types: Movie, Play, and Concert
+ * @author brandon Base Event type, will be extended by different event types: Movie, Play, and
+ *         Concert
  */
 public abstract class Event {
-	protected String name;
-	protected ArrayList<String> reviews;
-	protected int runTime;
-	protected String stars;
-	protected String genre;
-	protected String description;
-	
-	/**
-	 * Creates a new Event
-	 * @param name - name of the event - 
-	 */
-	public Event(String name) {
-		this.name = name;
-		
-		reviews = new ArrayList<>();
-	}
+  protected String name;
+  protected ArrayList<String> reviews;
+  protected int runTime;
+  protected String stars;
+  protected String genre;
+  protected String description;
+
+  /**
+   * Creates a new Event
+   * 
+   * @param name - name of the event -
+   */
+  public Event(String name) {
+    this.name = name;
+
+    reviews = new ArrayList<>();
+  }
+
+  public String getName() {
+    return this.name;
+  }
 }
