004dbc3c3492fd7523e01516834bab22cb27fcfd
c4181 <cm4181@gmail.com>
2020-04-06 10:27:01 -0400

    Added getters and filled out constructor

--- src/csce247_moive_theater/Movie.java
+++ src/csce247_moive_theater/Movie.java
@@ -1,19 +1,31 @@
 package csce247_moive_theater;
+
 /**
  * 
- * @author brandon
- * Extension of Event to create a Movie listing
+ * @author brandon Extension of Event to create a Movie listing
  */
-public class Movie extends Event{
-	private String mpaaRating;
-	
-	/**
-	 * Creates a new Movie listings
-	 * @param name - Name to be used by the super to name the event
-	 */
-	 
-	
-	public Movie(String name){
-		super(name);
-	}
+public class Movie extends Event {
+  private String mpaaRating;
+
+
+  /**
+   * Creates a new Movie listings
+   * 
+   * @param name - name of the event
+   * @param runTime - run time of the event
+   * @param stars - actor/performer staring the event
+   * @param genre - genre of the event
+   * @param description - short description of the event
+   * @param mpaaRating - mpaaRating for the Movie
+   */
+  public Movie(String name, int runTime, String stars, String genre, String description,
+      String mpaaRating) {
+    super(name, runTime, stars, genre, description);
+    this.mpaaRating = mpaaRating;
+  }
+
+
+  public String getMpaaRating() {
+    return mpaaRating;
+  }
 }
