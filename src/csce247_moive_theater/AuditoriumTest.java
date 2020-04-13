da86a034aaa97e77cc21006bd9627007cbc695d4
DivineWalker <divinew@email.sc.edu>
2020-04-13 17:30:35 -0400

    Adding Auditorium Test

---
+++ src/csce247_moive_theater/AuditoriumTest.java
@@ -0,0 +1,22 @@
+package csce247_moive_theater;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AuditoriumTest {
+
+	@Test
+	void testGetAuditoriumNumber() {
+		Auditorium aud = new Auditorium("1", new String[5][5]);
+		assertEquals(aud.getAuditoriumNumber(), "1");
+	}
+
+	@Test
+	void testGetSeatingMap() {
+		String[][] sm = new String[2][2];
+		Auditorium aud = new Auditorium("1", sm);
+		assertEquals(aud.getSeatingMap(), sm);
+	}
+
+}
