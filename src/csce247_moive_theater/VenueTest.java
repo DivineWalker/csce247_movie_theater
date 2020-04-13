ea904e4d2249d33282b41971afe81d7ed86abbcf
DivineWalker <divinew@email.sc.edu>
2020-04-13 17:31:42 -0400

    Adding Venue Test -- 2 Fails

---
+++ src/csce247_moive_theater/VenueTest.java
@@ -0,0 +1,67 @@
+package csce247_moive_theater;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+class VenueTest {
+
+	@Test
+	void testGetAuditorium() {
+		Venue vn = new Venue("USC Movie Theater", "123 Main St.", "Movie Theater", 5, 3, 5, new String[2][2], 5);
+		Auditorium ad = new Auditorium("3", new String[2][2]);
+		Assert.assertSame(ad, vn.getAuditorium("2"));
+	}
+
+	@Test
+	void testGetName() {
+		Venue vn = new Venue("USC Movie Theater", "123 Main St.", "Movie Theater", 5, 3, 5, new String[2][2], 5);
+		assertEquals(vn.getName(), "USC Movie Theater");
+	}
+
+	@Test
+	void testGetAddress() {
+		Venue vn = new Venue("USC Movie Theater", "123 Main St.", "Movie Theater", 5, 3, 5, new String[2][2], 5);
+		assertEquals(vn.getAddress(), "123 Main St.");
+	}
+
+	@Test
+	void testGetType() {
+		Venue vn = new Venue("USC Movie Theater", "123 Main St.", "Movie Theater", 5, 3, 5, new String[2][2], 5);
+		assertEquals(vn.getType(), "Movie Theater");
+	}
+
+	@Test
+	void testGetAdultTicketPrice() {
+		Venue vn = new Venue("USC Movie Theater", "123 Main St.", "Movie Theater", 5, 3, 5, new String[2][2], 5);
+		assertEquals(vn.getAdultTicketPrice(), 5);
+	}
+
+	@Test
+	void testGetChildTicketPrice() {
+		Venue vn = new Venue("USC Movie Theater", "123 Main St.", "Movie Theater", 5, 3, 5, new String[2][2], 5);
+		assertEquals(vn.getChildTicketPrice(), 3);
+	}
+
+	@Test
+	void testGetHandicapTicketPrice() {
+		Venue vn = new Venue("USC Movie Theater", "123 Main St.", "Movie Theater", 5, 3, 5.50, new String[2][2], 5);
+		assertEquals(vn.getHandicapTicketPrice(), 5.50);
+	}
+
+	@Test
+	void testGetAuditoriumList() {
+		String[][] sm = new String[2][2];
+		Venue vn = new Venue("USC Movie Theater", "123 Main St.", "Movie Theater", 5, 3, 5, sm, 2);
+		List<Auditorium> auditoriums = new ArrayList<>();
+		for(int i = 0; i < 2; i++)
+			auditoriums.add(new Auditorium(""+(i+1), sm));
+		assertEquals(vn.getAuditoriumList(), auditoriums);
+		
+	}
+
+}
