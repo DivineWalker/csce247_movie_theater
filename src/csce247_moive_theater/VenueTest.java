f25dba1139e79e952a820d56c8b7a59b8706f858
DivineWalker <divinew@email.sc.edu>
2020-04-14 11:19:21 -0400

    Completed Venue Test

--- src/csce247_moive_theater/VenueTest.java
+++ src/csce247_moive_theater/VenueTest.java
@@ -1,15 +1,19 @@
+/**
+ * @author Divine
+ */
 package csce247_moive_theater;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 class VenueTest {
 
+	/**
+	 * Test method for getAuditorium
+	 */
 	@Test
 	void testGetAuditorium() {
 		Venue vn = new Venue("USC Movie Theater", "123 Main St.", "Movie Theater", 5, 3, 5, new String[2][2], 5);
@@ -17,42 +21,63 @@ class VenueTest {
 		assertEquals(vn.getAuditorium("2").getAuditoriumNumber(), ad.getAuditoriumNumber());
 	}
 
+	/**
+	 * Test method for getName
+	 */
 	@Test
 	void testGetName() {
 		Venue vn = new Venue("USC Movie Theater", "123 Main St.", "Movie Theater", 5, 3, 5, new String[2][2], 5);
 		assertEquals(vn.getName(), "USC Movie Theater");
 	}
-
+	
+	/**
+	 * Test method for getAddress
+	 */
 	@Test
 	void testGetAddress() {
 		Venue vn = new Venue("USC Movie Theater", "123 Main St.", "Movie Theater", 5, 3, 5, new String[2][2], 5);
 		assertEquals(vn.getAddress(), "123 Main St.");
 	}
 
+	/**
+	 * Test method for getType
+	 */
 	@Test
 	void testGetType() {
 		Venue vn = new Venue("USC Movie Theater", "123 Main St.", "Movie Theater", 5, 3, 5, new String[2][2], 5);
 		assertEquals(vn.getType(), "Movie Theater");
 	}
 
+	/**
+	 * Test method for getAdultTicketPrice
+	 */
 	@Test
 	void testGetAdultTicketPrice() {
 		Venue vn = new Venue("USC Movie Theater", "123 Main St.", "Movie Theater", 5, 3, 5, new String[2][2], 5);
 		assertEquals(vn.getAdultTicketPrice(), 5);
 	}
 
+	/**
+	 * Test method for getChildTicketPrice
+	 */
 	@Test
 	void testGetChildTicketPrice() {
 		Venue vn = new Venue("USC Movie Theater", "123 Main St.", "Movie Theater", 5, 3, 5, new String[2][2], 5);
 		assertEquals(vn.getChildTicketPrice(), 3);
 	}
 
+	/**
+	 * Test method for getHandicapTicketPrice
+	 */
 	@Test
 	void testGetHandicapTicketPrice() {
 		Venue vn = new Venue("USC Movie Theater", "123 Main St.", "Movie Theater", 5, 3, 5.50, new String[2][2], 5);
 		assertEquals(vn.getHandicapTicketPrice(), 5.50);
 	}
 
+	/**
+	 * Test method for getAditoriumList
+	 */
 	@Test
 	void testGetAuditoriumList() {
 		String[][] sm = new String[2][2];
