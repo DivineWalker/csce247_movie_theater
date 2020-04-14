6d36677b6eb3e0a6a4641146663f6cda39823b46
Divine Walker <divine@Divines-MacBook-Pro.local>
2020-04-14 11:30:36 -0400

    Showtime Test Format Corrections

--- src/csce247_moive_theater/ShowtimeTest.java
+++ src/csce247_moive_theater/ShowtimeTest.java
@@ -6,7 +6,6 @@ package csce247_moive_theater;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
-
 /**
  * @author divinew
  *
@@ -44,8 +43,7 @@ class ShowtimeTest {
 		Event ev = new Event("Frozen", 105, "5", "comedy", "Frozen movie", "Movie", "5");
 		Showtime sh = new Showtime(vn, ev, "2", "12:00");
 		assertEquals(sh.getAvailableSeats(), vn.getAuditorium("2").getSeatingMap());
-		
-		
+
 	}
 
 	/**
@@ -78,7 +76,7 @@ class ShowtimeTest {
 		Venue vn = new Venue("USC Movie Theater", "123 Main St.", "Movie Theater", 5, 3, 5, new String[2][2], 5);
 		Event ev = new Event("Frozen", 105, "5", "comedy", "Frozen movie", "Movie", "5");
 		Showtime sh = new Showtime(vn, ev, "2", "12:00");
-		sh.takeSeat(0,0);
+		sh.takeSeat(0, 0);
 		assertEquals(sh.getAvailableSeats()[0][0], " ");
 	}
 
@@ -90,7 +88,8 @@ class ShowtimeTest {
 		Venue vn = new Venue("USC Movie Theater", "123 Main St.", "Movie Theater", 5, 3, 5, new String[2][2], 5);
 		Event ev = new Event("Frozen", 105, "5", "comedy", "Frozen movie", "Movie", "5");
 		Showtime sh = new Showtime(vn, ev, "2", "12:00");
-		assertEquals(sh.toString(),"Venue: USC Movie Theater\nEvent: Frozen\nTime of Show: 12:00\nAuditorium Number: 2");
+		assertEquals(sh.toString(),
+				"Venue: USC Movie Theater\nEvent: Frozen\nTime of Show: 12:00\nAuditorium Number: 2");
 	}
 
 }
